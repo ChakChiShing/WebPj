3ac988e6a87a78737899e6bf3191a7a9ee913693
ChakChiShing <62586452+ChakChiShing@users.noreply.github.com>
2020-08-10 17:31:37 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import CreateTodo from "./components/create-todo.component";
 import TodosList from "./components/todos-list.component";
 import ProjectInfo from "./components/projectInfo.component";
+import EditPJInfo from "./components/editPj.component";
 
 class App extends Component  {
   render() {
@@ -26,6 +27,7 @@ class App extends Component  {
           <Route path="/" exact component={TodosList} />
           <Route path="/create" component={CreateTodo} />
           <Route path="/project/:id" component={ProjectInfo} />
+          <Route path="/edit/:id" component={EditPJInfo} />
         </div>
       </Router>
     );
