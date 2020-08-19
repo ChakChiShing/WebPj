048ba9f1709ee4e27dbd5624b25865d5903ea7b4
ixxsth <66905831+ixxsth@users.noreply.github.com>
2020-08-19 15:41:27 +0800

    Update functions.js

--- src/components/functions.js
+++ src/components/functions.js
@@ -62,11 +62,19 @@ export const CostInput = (props) => {
       <div className='input-group-prepend'>
         <div className='input-group-text'>$</div>
       </div>
-      <input type="number" className="form-control" placeholder={props.placeholder}
+      <input type="number" className="form-control" placeholder='Cost'
         value={props.cost}
-        name={props.name}
+        name='cost'
         onChange={(e) => props.func(e.target.name, e.target.value, props.obj, props.elem)}
       />
     </div>
+  )
+}
+
+export const QuantityInput = (props) => {
+  return(
+    <input type="number" name='quantity' className="form-control" placeholder='Quantity' min='1'
+      value={props.quantity} onChange={(e) => props.func(e.target.name, e.target.value, props.obj, props.elem)}
+    />
   )
 }
